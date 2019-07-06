cc315c4f88ba9d294b0006a5b2debad3e50650a7
yoowonsuk <sam2yuk@naver.com>
2019-07-06 21:20:32 +0900

    6th july 2019 at night

---
+++ 3-2/q1.py
@@ -0,0 +1,3 @@
+n1 = eval(input("First input: "))
+n2 = eval(input("Second input: "))
+print("sum of two numbers is", n1 + n2)
---
+++ 3-3/q3.py
@@ -0,0 +1,4 @@
+for i in range(1, 10):
+    print("7 *", i, "=", 7 * i)
+
+
