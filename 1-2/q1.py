2eca6f0fe91fb6159db796aefab3327e0b62af1c
yoowonsuk <sam2yuk@naver.com>
2019-07-06 18:25:39 +0900

    6th july 2019

---
+++ 1-2/q1.py
@@ -0,0 +1,8 @@
+x = 2 * 2 * 2
+print(x)
+
+y = x / 4
+print(y)
+
+z = y * y
+print(z)
---
+++ 2-1/q1.py
@@ -0,0 +1,7 @@
+def MH():
+    print("1 + 2 + 3 + 4 + 5 = ", 1 + 2 + 3 + 4 + 5)
+    print("Simple is the best!")
+    print("The happy python~")
+
+MH()    # erase an indentation
+MH()
